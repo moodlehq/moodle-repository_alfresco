023c48ace9449b0943365dc05da7d3c5652513b3
Andrew Nicols <andrew@nicols.co.uk>
2016-07-12 09:03:39 +0800

    Update version to 2016071200

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016071100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016071200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'repository_alfresco'; // Full name of the plugin (used for diagnostics)
-$plugin->release = '3.2dev (2016071100)';
+$plugin->release = '3.2dev (2016071200)';
 $plugin->maturity = MATURITY_STABLE;
